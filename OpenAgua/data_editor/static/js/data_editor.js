2ea9b423bfaba38aa1f310f22696d4b18e46b0c7
David Rheinheimer <herr.rhein@gmail.com>
2016-08-23 13:01:09 -0500

    improvements to data editor but still not working

---
+++ OpenAgua/data_editor/static/js/data_editor.js
@@ -0,0 +1,74 @@
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/chrome");
+editor.getSession().setMode("ace/mode/python");
+document.getElementById('editor').style.fontSize='16px';
+
+var feature_id;
+var scenario_id;
+var template_id;
+var attr_id;
+var type_id;
+$(document).ready(function(){
+
+  // load the scenarios
+  $('#scenarios').on('hide.bs.select', function (e) {
+    scenario_id = Number($('#scenarios option:selected').attr("data-tokens"));
+    $('#features').attr('disabled',false);
+    $('#features').selectpicker('refresh');
+  });
+
+  // load the variables when the feature is clicked
+  $('#features').on('hide.bs.select', function (e) {
+    var data_tokens = $('#features option:selected').attr("data-tokens");
+    var feature_data = $.parseJSON(data_tokens);
+    type_id = feature_data.type_id;
+    feature_id = feature_data.feature_id;
+    feature_type = feature_data.feature_type;
+    if (!isNaN(feature_id)) {
+      load_variables(type_id);
+    };    
+  });
+
+  // load the variable data when the variable is clicked
+  $('#variables').on('hide.bs.select', function (e) {
+    attr_id = Number($('#variables option:selected').attr("data-tokens"));
+    if (!isNaN(attr_id)) {
+      load_data(feature_id, feature_type, attr_id, scenario_id);
+    };    
+  });  
+  
+});
+
+// load the variables (aka attributes in Hydra)
+function load_variables(type_id) {
+  var data = {type_id: type_id}
+  $.getJSON($SCRIPT_ROOT+'/_get_variables', data, function(resp) {
+      var variables = resp.result;
+      var vpicker = $('#variables')
+      vpicker.empty();
+      $.each(variables, function(index, variable) {
+        vpicker
+          .append($('<option>')
+            .attr('data-tokens',variable.attr_id)
+            .text(variable.attr_name.replace(/_/g,' '))
+          );
+      });
+      vpicker.attr('disabled',false);
+      vpicker.selectpicker('refresh');
+  });
+};
+
+// load the variable data
+function load_data(feature_id, feature_type, attr_id, scenario_id) {
+  var data = {
+    type_id: type_id,
+    feature_type: feature_type,
+    feature_id: feature_id,
+    attr_id: attr_id,
+    scen_id: scenario_id
+  };
+  $.getJSON($SCRIPT_ROOT+'_get_variable_data', data, function(resp) {
+    var attr_data = resp.attr_data;
+    editor.setValue(attr_data);
+  });
+};
--- OpenAgua/data_editor/views.py
+++ OpenAgua/data_editor/views.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from flask import render_template, request, session, jsonify
 from flask_user import login_required
 from ..connection import connection
@@ -10,25 +11,38 @@ from . import data_editor
 def data_editor_main():    
     conn = connection(url=session['url'], session_id=session['session_id'])
     network = conn.call('get_network', {'network_id':session['network_id'],'include_data':'N'})
+    template = conn.call('get_template',{'template_id':session['template_id']})
+    features = OrderedDict()
+    for t in template.types:
+        resources = conn.call('get_resources_of_type', {'network_id':network.id, 'type_id':t.id})
+        if resources:
+            features[(t.id, t.name, t.resource_type)] = resources
     
     return render_template('data_editor.html',
-                           nodes = network.nodes,
-                           links = network.links,
-                           scenarios = network.scenarios)
+                           features=features,
+                           scenarios=network.scenarios)
 
 @data_editor.route('/_get_variables', methods=['GET','POST'])
 @login_required
 def get_variables():
     conn = connection(url=session['url'], session_id=session['session_id'])
-    feature_type = request.args.get('feature_type')
-    feature_id = int(request.args.get('feature_id'))
-    
-    # this can be made more efficient with the use of get_resources_by_type in listing features
-    # in data_editor, since then we can 1) organize by resource_type and 2) already have the resource type ID
-    feature = conn.call('get_'+feature_type, {feature_type+'_id':feature_id})
-    restypes = feature.types
-    type_id = [t.id for t in restypes if t.template_id==session['template_id']][0]
+    type_id = int(request.args.get('type_id'))
     type_ = conn.call('get_templatetype', {'type_id':type_id})
     attrs = [a for a in type_.typeattrs if a.is_var=='N']
     
     return jsonify(result=attrs)
+
+@data_editor.route('/_get_variable_data')
+def get_variable_data():
+    conn = connection(url=session['url'], session_id=session['session_id'])
+    
+    feature_type = request.args.get('feature_type').lower()
+    feature_id = int(request.args.get('feature_id'))
+    attr_id = int(request.args.get('attr_id'))
+    scen_id = int(request.args.get('scen_id'))
+    type_id = int(request.args.get('type_id'))
+
+    args = {'%s_id' % feature_type: feature_id, 'scenario_id': scen_id, 'type_id': type_id}
+    data = conn.call('get_%s_data' % feature_type, args)
+    
+    return jsonify(results=data)
