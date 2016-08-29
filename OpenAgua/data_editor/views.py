00a8daaf04e5930ab9e21821baf4d75534936538
David Rheinheimer <herr.rhein@gmail.com>
2016-08-29 09:50:26 -0500

    deleted attr_dict global - python globals don't work in flask

--- OpenAgua/data_editor/views.py
+++ OpenAgua/data_editor/views.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from flask import render_template, request, session, jsonify, json
+from flask import render_template, request, session, jsonify, json, g
 from flask_user import login_required, current_user
 from ..connection import connection
 from ..evaluator import evaluate
@@ -7,13 +7,12 @@ from ..evaluator import evaluate
 # import blueprint definition
 from . import data_editor
 
-global attr_dict
-
 @data_editor.route('/data_editor')
 @login_required
 def data_editor_main():    
     conn = connection(url=session['url'], session_id=session['session_id'])
-    network = conn.call('get_network', {'network_id':session['network_id'],'include_data':'N'})
+    network = conn.call('get_network', {'network_id':session['network_id'],
+                                        'include_data':'N'})
     template = conn.call('get_template',{'template_id':session['template_id']})
     features = OrderedDict()   
     
@@ -22,16 +21,12 @@ def data_editor_main():
             if ttype.resource_type=='NETWORK':
                 pass # not sure how to load these
             elif ttype.resource_type==res_type:
-                feats = [r for r in eval('network.{}s'.format(res_type.lower())) if ttype.id in [t.id for t in r.types]]
+                feats = [r for r in eval('network.{}s' \
+                                         .format(res_type.lower())) \
+                         if ttype.id in [t.id for t in r.types]]
                 if feats:
-                    features[(ttype.id, ttype.name.replace('_', ' '), ttype.resource_type)] = feats        
-            
-    # create an attribute lookup dictionary
-    global attr_dict
-    attrs = conn.call('get_template_attributes', {'template_id':session['template_id']})
-    attr_dict = {}
-    for a in attrs:
-        attr_dict[a.id] = a.name    
+                    features[(ttype.id, ttype.name.replace('_', ' '),
+                              ttype.resource_type)] = feats
     
     return render_template('data_editor.html',
                            features=features,
@@ -40,13 +35,21 @@ def data_editor_main():
 @data_editor.route('/_get_variables', methods=['GET','POST'])
 @login_required
 def get_variables():
-    global attr_dict
     
     conn = connection(url=session['url'], session_id=session['session_id'])
     type_id = int(request.args.get('type_id'))
     feature_id = int(request.args.get('feature_id'))
     feature_type = request.args.get('feature_type').lower()
-    res_attrs = conn.call('get_%s_attributes'%feature_type, {'%s_id'%feature_type: feature_id, 'type_id':type_id})
+    res_attrs = conn.call('get_%s_attributes'%feature_type,
+                          {'%s_id'%feature_type: feature_id, 'type_id':type_id})
+    
+    # create an attribute lookup dictionary - this is inefficient
+    # we should do this just once per application context
+    attrs = conn.call('get_template_attributes',
+                      {'template_id':session['template_id']})
+    attr_dict = {}
+    for a in attrs:
+        attr_dict[a.id] = a.name    
     
     # add a name to each resource_attr based on the associated attribute id
     for i in range(len(res_attrs)):
@@ -66,7 +69,9 @@ def get_variable_data():
     scen_id = int(request.args.get('scen_id'))
     type_id = int(request.args.get('type_id'))
 
-    args = {'%s_id' % feature_type: feature_id, 'scenario_id': scen_id, 'type_id': type_id}
+    args = {'%s_id' % feature_type: feature_id,
+            'scenario_id': scen_id,
+            'type_id': type_id}
     feature_data = conn.call('get_%s_data' % feature_type, args)
     
     attr_data = [row for row in feature_data if row.attr_id==attr_id]
@@ -93,8 +98,8 @@ def add_variable_data():
     scen_id = int(request.args.get('scen_id'))
     val = request.args.get('val')
     
-    # we need to create Dataset. Since we are attaching it to an existing attribute, we will
-    # use the same dimensions, units, etc. as that attribute.
+    # we need to create Dataset. Since we are attaching it to an existing
+    # attribute, we will use the same dimensions, units, etc. as that attribute.
     attr = conn.call('get_attribute_by_id', {'ID':attr_id})
     
     dataset = dict(
@@ -107,7 +112,9 @@ def add_variable_data():
         metadata = json.dumps({'source':'OpenAgua/%s' % current_user.username})
     )    
     
-    args = {'scenario_id': scen_id, 'resource_attr_id': res_attr_id, 'dataset': dataset}
+    args = {'scenario_id': scen_id,
+            'resource_attr_id': res_attr_id,
+            'dataset': dataset}
     result = conn.call('add_data_to_attribute', args)
     if 'faultcode' in result.keys():
         status = -1
@@ -127,9 +134,12 @@ def _update_variable_data():
     scen_id = int(request.args.get('scen_id'))
     dataset = request.args.get('attr_data')
     dataset = json.loads(dataset)
-    dataset['metadata'] = json.dumps({'source':'OpenAgua/%s' % current_user.username})
+    dataset['metadata'] = \
+        json.dumps({'source':'OpenAgua/%s' % current_user.username})
     
-    args = {'scenario_id': scen_id, 'resource_attr_id': dataset['resource_attr_id'], 'dataset': dataset['value']}
+    args = {'scenario_id': scen_id,
+            'resource_attr_id': dataset['resource_attr_id'],
+            'dataset': dataset['value']}
     result = conn.call('add_data_to_attribute', args)
     if 'faultcode' in result.keys():
         status = -1
