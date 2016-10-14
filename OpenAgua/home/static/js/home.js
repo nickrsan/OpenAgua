c4cecfeceb759c1ff9316091b1acc6776b21f831
David Rheinheimer <herr.rhein@gmail.com>
2016-10-14 15:19:11 -0500

    Merge remote-tracking branch 'refs/remotes/origin/master' into dev-david
    # Conflicts:
    #	OpenAgua/home/static/js/home.js

--- OpenAgua/home/static/js/home.js
+++ OpenAgua/home/static/js/home.js
@@ -203,16 +203,15 @@ var network_dropdown = make_button_div('network', network_actions);
 // update network list
 function update_networks(active_project_id, active_network_id) {
 
-  if (active_project_id) {
-    var func = 'get_networks'
-    var args = {'project_id': active_project_id, 'include_data':'N'}
-    $.getJSON($SCRIPT_ROOT + '/_hydra_call', {func: func, args: JSON.stringify(args)}, function(resp) {
-      var networks = resp.result;
+  var func = 'get_networks'
+  var args = {'project_id': active_project_id, 'include_data':'N'}
+  $.getJSON($SCRIPT_ROOT + '/_hydra_call', {func: func, args: JSON.stringify(args)}, function(resp) {
+    if ('faultcode' in resp.result) {
+      $("#network_list").empty().text("No project loaded.")
+    } else {
       populate_networks(networks);
-    }); 
-  } else {
-    $("#network_list").empty().text("Select a project first.")
-  }
+    }
+  }); 
 }
 
 function populate_networks(networks) {
