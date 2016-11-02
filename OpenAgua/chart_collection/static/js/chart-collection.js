4e6e419671e80afdb45998813ba4d217b187b78c
David Rheinheimer <herr.rhein@gmail.com>
2016-11-02 11:01:35 -0400

    added lightbox gallery

--- OpenAgua/chart_collection/static/js/chart-collection.js
+++ OpenAgua/chart_collection/static/js/chart-collection.js
@@ -2,18 +2,23 @@
 
 // load selected chart
 $(function() {
-    
+
     //$(".edit").click( function() {
         //var chartId = Number($(this).parent().attr('id'));
         //$.getJSON( "/_load_chart", { chart_id: chartId }, function(resp) {
             //window.location = resp.redirect;
         //});
     //});
-    
+
     $('#thumbnails').magnificPopup({
-      delegate: 'a', // child items selector, by clicking on it popup will open
-      type: 'image'
-      // other options
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [1,1]
+            }
     });
-    
+
 });
