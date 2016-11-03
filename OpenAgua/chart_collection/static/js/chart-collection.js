28c0c371bad2058840c16b465560073d6549f306
David Rheinheimer <herr.rhein@gmail.com>
2016-11-03 10:37:19 -0400

    updates to chart system

--- OpenAgua/chart_collection/static/js/chart-collection.js
+++ OpenAgua/chart_collection/static/js/chart-collection.js
@@ -3,12 +3,12 @@
 // load selected chart
 $(function() {
 
-    //$(".edit").click( function() {
-        //var chartId = Number($(this).parent().attr('id'));
-        //$.getJSON( "/_load_chart", { chart_id: chartId }, function(resp) {
-            //window.location = resp.redirect;
-        //});
-    //});
+    $(".edit").click( function() {
+        var chartId = $(this).data('id');
+        $.getJSON( "/_load_chart", { chart_id: chartId }, function(resp) {
+            window.location = resp.redirect;
+        });
+    });
 
     $('#thumbnails').magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
@@ -18,7 +18,32 @@ $(function() {
             enabled: true,
             navigateByImgClick: true,
             preload: [1,1]
-            }
+        }
     });
+    
+    $('.trash').click( function() {
+        var chart_id = $(this).data('id');
+        var col = $(this).closest(".col");
+        bootbox.confirm('Are you sure you want to delete this chart?',
+            function(confirm) {
+                if (confirm) {
+                     $.ajax({
+                        type : "POST",
+                        url : '_delete_chart',
+                        data: JSON.stringify({chart_id: chart_id}),
+                        contentType: 'application/json',
+                        success: function(resp) {
+                            if (resp.result === 1) {
+                                col.remove();
+                                notify('success', 'Success!', 'Chart deleted.');
+                            } else {
+                                notify('warning', 'Failure!', 'Something went wrong. Chart not deleted.');
+                            }
+                        }
+                    });
+                }
+            }
+        );
+    })
 
 });
